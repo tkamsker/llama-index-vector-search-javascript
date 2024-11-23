e2222934c01566baa743528f638a12c831a4b70f
Wassim Chegham <github@wassim.dev>
2024-11-23 23:37:16 +0100

    fix: update app auth and managed identity

--- app/api/chat/engine/settings.ts
+++ app/api/chat/engine/settings.ts
@@ -1,15 +1,19 @@
 import {
   DefaultAzureCredential,
   getBearerTokenProvider,
+  ManagedIdentityCredential,
 } from "@azure/identity";
 import {
   KnownAnalyzerNames,
   KnownVectorSearchAlgorithmKind,
 } from "@azure/search-documents";
 
-import { OpenAI, OpenAIEmbedding, Settings } from "llamaindex";
-
-import { FilterableMetadataFieldKeysType } from "llamaindex";
+import {
+  FilterableMetadataFieldKeysType,
+  OpenAI,
+  OpenAIEmbedding,
+  Settings,
+} from "llamaindex";
 
 // FIXME: import from 'llamaindex'
 import {
@@ -19,34 +23,66 @@ import {
 
 const CHUNK_SIZE = 512;
 const CHUNK_OVERLAP = 20;
+const AZURE_COGNITIVE_SERVICES_SCOPE =
+  "https://cognitiveservices.azure.com/.default";
 
 export const initSettings = async () => {
   if (
-    !process.env.AZURE_OPENAI_DEPLOYMENT ||
+    !process.env.AZURE_OPENAI_CHAT_DEPLOYMENT ||
     !process.env.AZURE_OPENAI_EMBEDDING_DEPLOYMENT
   ) {
     throw new Error(
-      "'AZURE_OPENAI_DEPLOYMENT' and 'AZURE_OPENAI_EMBEDDING_DEPLOYMENT' env variables must be set.",
+      "'AZURE_OPENAI_CHAT_DEPLOYMENT' and 'AZURE_OPENAI_EMBEDDING_DEPLOYMENT' env variables must be set.",
     );
   }
 
-  const azure: any = {
-    deployment: process.env.AZURE_OPENAI_DEPLOYMENT ?? "gpt-4",
-  };
+  let credential;
+  let openAiApiKey;
+  let azureSearchApiKey;
 
   if (process.env.OPENAI_API_KEY) {
-    azure.apikey = process.env.OPENAI_API_KEY as string;
-  } else {
-    const credential = new DefaultAzureCredential();
-    const azureADTokenProvider = getBearerTokenProvider(credential, [
-      "https://cognitiveservices.azure.com/.default",
-      "https://search.azure.com/.default",
-    ]);
-    azure.azureADTokenProvider = azureADTokenProvider;
+    // Authenticate using an Azure OpenAI API key
+    // This is generally discouraged, but is provided for developers
+    // that want to develop locally inside the Docker container.
+    openAiApiKey = process.env.OPENAI_API_KEY;
+    console.log("Using OpenAI API key for authentication");
   }
+
+  if (process.env.AZURE_AI_SEARCH_API_KEY) {
+    // Authenticate using an Azure AI Search API key
+    // This is generally discouraged, but is provided for developers
+    // that want to develop locally inside the Docker container.
+    azureSearchApiKey = process.env.AZURE_AI_SEARCH_API_KEY;
+  }
+
+  if (process.env.AZURE_CLIENT_ID) {
+    // Authenticate using a user-assigned managed identity on Azure
+    // See infra/main.bicep for value of AZURE_OPENAI_CLIENT_ID
+    credential = new ManagedIdentityCredential({
+      clientId: process.env.AZURE_CLIENT_ID,
+    });
+    console.log("Using managed identity for authentication");
+    console.log({ clientId: process.env.AZURE_CLIENT_ID });
+  } else {
+    // Authenticate using the default Azure credential chain
+    // See https://learn.microsoft.com/en-us/azure/developer/javascript/sdk/authentication/overview#use-defaultazurecredential-in-an-application
+    // This will *not* work inside a Docker container.
+    credential = new DefaultAzureCredential();
+    console.log("Using default Azure credential chain for authentication");
+  }
+
+  const azureADTokenProvider = getBearerTokenProvider(
+    credential,
+    AZURE_COGNITIVE_SERVICES_SCOPE,
+  );
+  const azure = {
+    azureADTokenProvider,
+    deployment: process.env.AZURE_OPENAI_CHAT_DEPLOYMENT,
+  };
 
   // configure LLM model
   Settings.llm = new OpenAI({
+    apiKey: openAiApiKey,
     azure,
   });
 
@@ -64,25 +100,23 @@ export const initSettings = async () => {
   // generation pipelines
   (Settings as any).__AzureAISearchVectorStoreInstance__ =
     new AzureAISearchVectorStore({
+      key: azureSearchApiKey,
+      credential: credential as unknown as DefaultAzureCredential,
       indexName:
-        process.env.AZURE_SEARCH_INDEX_NAME ?? "llamaindex-vector-search",
-      filterableMetadataFieldKeys:
-        [] as unknown as FilterableMetadataFieldKeysType,
-
+        process.env.AZURE_AI_SEARCH_INDEX ?? "llamaindex-vector-search",
       // FIXME: import IndexManagement.CREATE_IF_NOT_EXISTS from 'llamaindex'
       // indexManagement: IndexManagement.CREATE_IF_NOT_EXISTS,
       indexManagement: "CreateIfNotExists" as IndexManagement,
-      idFieldKey: "id",
-      chunkFieldKey: "chunk",
-      embeddingFieldKey: "embedding",
-      metadataStringFieldKey: "metadata",
-      docIdFieldKey: "doc_id",
-      embeddingDimensionality: 1536,
+      idFieldKey: process.env.AZURE_AI_SEARCH_ID_FIELD ?? "id",
+      chunkFieldKey: process.env.AZURE_AI_SEARCH_CHUNK_FIELD ?? "chunk",
+      embeddingFieldKey:
+        process.env.AZURE_AI_SEARCH_EMBEDDING_FIELD ?? "embedding",
+      metadataStringFieldKey:
+        process.env.AZURE_AI_SEARCH_METADATA_FIELD ?? "metadata",
+      docIdFieldKey: process.env.AZURE_AI_SEARCH_DOC_ID_FIELD ?? "doc_id",
+      embeddingDimensionality: 3072,
       languageAnalyzer: KnownAnalyzerNames.EnLucene,
       // store vectors on disk
       vectorAlgorithmType: KnownVectorSearchAlgorithmKind.ExhaustiveKnn,
-
-      // Optional: Set to "scalar" or "binary" if using HNSW
-      // compressionType: KnownVectorSearchCompressionKind.BinaryQuantization,
     });
 };
